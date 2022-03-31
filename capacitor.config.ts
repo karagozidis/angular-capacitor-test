a2fde62698049f4ef98a82a23864a8aca5c95abd
heliaskaragozidis <!1Helias!!>
2022-03-31 15:22:16 +0300

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ang.captest',
+  appName: 'angular-capacitor-test',
+  webDir: 'dist/angular-capacitor-test',
+  bundledWebRuntime: false
+};
+
+export default config;
